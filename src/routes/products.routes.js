10524c462a36eb6779983980a01ffad9b26062ec
akashbandla <akashbandla73@gmail.com>
2026-09-12 10:43:40 +0530

    feat: add product retrieval with role-based access

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 
-import { createProduct } from '../controllers/products.controller.js';
+import { createProduct, getProductById, getProducts } from '../controllers/products.controller.js';
+import adminOnly from '../middlewares/role.middleware.js';
 
 const router = express.Router();
 
+router.get('/', getProducts);
+router.get('/:id', getProductById);
+
+router.use(adminOnly);
 router.post('/create', createProduct);
 
 export default router;
